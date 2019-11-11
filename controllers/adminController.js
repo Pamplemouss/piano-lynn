acc012b17d7bd56086ce57b1e1e590cfd0a827a3
Pamplemouss <john.dufaye@gmail.com>
2019-11-11 03:27:16 +0100

    Fix missing folder
    Create data folder and results.json if not exist.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,21 @@
 module.exports.admin = function(req, res) {
   const fs = require('fs');
+  var path = "data/results.json";
+  var data;
 
-  var data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  // check if folder exists
+  if (!fs.existsSync("data")) {
+    fs.mkdirSync("data");
+  }
+
+  // check if results.json exists
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify({"time":[]}));
+    data = {"time":[]};
+  }
+  else {
+    data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  }
 
   res.render('admin.ejs', {data: data});
 }
@@ -9,9 +23,7 @@ module.exports.admin = function(req, res) {
 
 module.exports.deleteData = function(req, res) {
   const fs = require('fs');
-
-  var data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
-  data.time = [];
+  var data = {"time": []};
 
   fs.writeFileSync('data/results.json', JSON.stringify(data));
   res.sendStatus(200);
--- controllers/pianoLynnController.js
+++ controllers/pianoLynnController.js
@@ -5,10 +5,33 @@ module.exports.pianoLynn = function(req, res) {
 module.exports.saveResults = function(req, res) {
   const fs = require('fs');
   var newTime = req.body.averageTime;
-  var data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  var path = "data/results.json";
+  var data;
+
+  // check if folder exists
+  if (!fs.existsSync("data")) {
+    fs.mkdirSync("data");
+  }
+
+  // check if results.json exists
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify({"time":[]}));
+    data = {"time":[]};
+  }
+  else {
+    data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  }
+
+
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync("data");
+    data = {"time":[]};
+  }
+  else {
+    data = JSON.parse(fs.readFileSync('data/results.json', 'utf8'));
+  }
 
   data.time.push(newTime);
-
   fs.writeFileSync('data/results.json', JSON.stringify(data));
 
   res.sendStatus(200);
